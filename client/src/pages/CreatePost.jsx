742dcc768784441df4f19c6df1ca3eb78197c899
Tania Zaiets <tetiana.zaiets.pz.2021@lpnu.ua>
2024-06-05 22:58:47 +0300

    add show more functionality to the posts results of the dashboard

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -114,6 +114,7 @@ export default function CreatePost() {
                         <option value="python">Python</option>
                         <option value="java">Java</option>
                         <option value="reactjs">React.js</option>
+                        <option value="nextjs">Next.js</option>
                     </Select>
                 </div>
                 <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
